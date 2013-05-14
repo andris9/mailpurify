504f3c68e7a42d09a58264c83b389da3a8ecb3aa
Andris Reinman <andris@kreata.ee>
2013-05-14 16:30:28 +0300

    remove HTML part, do not use callback

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var indentText = require("./lib/text").indentText;
-
-module.exports.indentText = function(text, callback){
-    callback(null, indentText(text));
-};
-
-module.exports.indentHTML = require("./lib/html").indentHTML;
+module.exports.indentText = require("./lib/text").indentText;
